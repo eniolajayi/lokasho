7cd99f311d66d57585cb2d0cffe2e191d1b37458
Eniola Ajayi <ajayieniola16@gmail.com>
2026-05-09 07:28:31 +0100

    adjust drizzle connection setup

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,14 +1,12 @@
+import "dotenv/config";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
-import * as schema from ".";
 
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
   throw new Error("DATABASE_URL enviroment variable is required");
 }
-
-const client = postgres(connectionString);
-
-export const db = drizzle(client, { schema });
+const queryClient = postgres(connectionString);
+export const db = drizzle({ client: queryClient });
 export type Db = typeof db;
