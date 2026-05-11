cc49b8ec1a484ad475dcf1a500d06ebd0b0df3a6
Eniola Ajayi <ajayieniola16@gmail.com>
2026-05-11 05:14:56 +0100

    refactor weatherService

--- api/src/services/weatherService.ts
+++ api/src/services/weatherService.ts
@@ -3,14 +3,11 @@ import { weatherCache } from "../db/schema";
 import { eq } from "drizzle-orm";
 import { fetchWeatherApi } from "openmeteo";
 
-const CACHE_TTL_MINUTES = 30;
-const OPEN_METEO_URL = "https://api.open-meteo.com/v1/forecast";
-
-export interface WeatherData {
+export interface CustomWeatherForecast {
   temperature: number;
   humidity: number;
   windSpeed: number;
-  // uvIndex: number;
+  uvIndex: number;
   rainProbability: number;
   hourly: HourlyForecast[];
 }
@@ -24,96 +21,75 @@ export interface HourlyForecast {
   rainProbability: number;
 }
 
-function buildCacheKey(lat: number, lon: number): string {
-  // round to 2dp so nearby locations share cache entries
-  return `${lat.toFixed(2)}:${lon.toFixed(2)}`;
-}
-
-// async function fetchFromOpenMeteo(
-//   lat: number,
-//   lon: number,
-// ): Promise<WeatherData> {
-//   const params = new URLSearchParams({
-//     latitude: lat.toString(),
-//     longitude: lon.toString(),
-
-//     current: [
-//       "temperature_2m",
-//       "relative_humidity_2m",
-//       "wind_speed_10m",
-//       "uv_index",
-//       "precipitation_probability",
-//     ].join(","),
-//     hourly: [
-//       "temperature_2m",
-//       "relative_humidity_2m",
-//       "wind_speed_10m",
-//       "uv_index",
-//       "precipitation_probability",
-//     ].join(","),
-//     forecast_days: "1",
-//     timezone: "auto",
-//   });
-
-//   const res = await fetch(`https://api.open-meteo.com/v1/forecast?${params}`);
-
-//   if (!res.ok) {
-//     throw new Error(`Open-Meteo error: ${res.status} ${res.statusText}`);
-//   }
-
-//   // figure out the shape of open meteo response data
-//   const raw = await res.json();
-//   const c = raw.current;
-//   const hourly: HourlyForecast[] = raw.hourly;
-
-//   return {
-//     temperature: c.temperature_2m,
-//     humidity: c.relative_humidity_2m,
-//     windSpeed: c.wind_speed_10m,
-//     uvIndex: c.uv_index,
-//     rainProbability: c.precipitation_probability,
-//     hourly,
-//   };
-// }
-
-async function fetchFromOpenMeteo(lat: number, lon: number) {
-  const params = new URLSearchParams({
-    latitude: lat.toString(),
-    longitude: lon.toString(),
-
-    // The info needed from open meteo
-    current: [
-      "temperature_2m",
-      "relative_humidity_2m",
-      "wind_speed_10m",
-      "uv_index",
-      "precipitation_probability",
-    ].join(","),
-    hourly: [
-      "temperature_2m",
-      "relative_humidity_2m",
-      "wind_speed_10m",
-      "uv_index",
-      "precipitation_probability",
-    ].join(","),
-    forecast_days: "1",
-    timezone: "auto",
-  });
-
-  const responses = await fetchWeatherApi(OPEN_METEO_URL, params);
-
-  return responses[0];
-}
-
-export async function getWeatherData(
+/// get custom weather forecast from cache or re-fetch
+export async function getWeather(
   lat: number,
   lon: number,
-): Promise<WeatherData> {
-  const raw = await fetchFromOpenMeteo(lat, lon);
+): Promise<CustomWeatherForecast> {
+  const cacheKey = buildCacheKey(lat, lon);
+
+  // retrieve from cache first.
+  const cached = await db
+    .select()
+    .from(weatherCache)
+    .where(eq(weatherCache.id, cacheKey))
+    .limit(1);
+
+  if (cached.length > 0) {
+    const entry = cached[0];
+    // check if its expired
+    if (new Date() < new Date(entry.expiresAt)) {
+      return entry.data as CustomWeatherForecast;
+    }
+  }
+
+  // if no cached/expired forecast, fetch fresh data and cache it
+  const data = await fetchCustomWeatherForecast(lat, lon);
+  await addForecastToCache(cacheKey, lat, lon, data);
+  return data;
+}
+
+async function addForecastToCache(
+  key: string,
+  lat: number,
+  lon: number,
+  data: CustomWeatherForecast,
+) {
+  const CACHE_TTL_MINUTES = 30; //minutes
+  const expiresAt = new Date(Date.now() + CACHE_TTL_MINUTES * 60 * 1000);
+  await db
+    .insert(weatherCache)
+    .values({
+      id: key,
+      lat,
+      lon,
+      data,
+      expiresAt,
+    })
+    // upsert
+    .onConflictDoUpdate({
+      target: weatherCache.id,
+      set: { data, fetchedAt: new Date(), expiresAt },
+    });
+}
+
+function buildCacheKey(lat: number, lon: number): string {
+  // round to 2dp so nearby locations share cache entries
+  return `lksh-wth-che-${lat.toFixed(2)}:${lon.toFixed(2)}`;
+}
+
+// we receive so much information from the weather api.
+// restructure to get only what we need in a nice format.
+async function fetchCustomWeatherForecast(
+  lat: number,
+  lon: number,
+): Promise<CustomWeatherForecast> {
+  const raw = await fetchFromExternalAPI(lat, lon);
   const current = raw.current();
   const hourly = raw.hourly();
+  const daily = raw.daily();
 
-  if (current === null || hourly === null) {
+  if (current === null || hourly === null || daily === null) {
     throw new Error(
       `No 'current' weather data and hourly weather data received from open-meteo response.`,
     );
@@ -121,6 +97,7 @@ export async function getWeatherData(
 
   const utcOffsetSeconds = raw.utcOffsetSeconds();
 
+  //
   const hourlyTimes = Array.from(
     {
       length:
@@ -139,6 +116,7 @@ export async function getWeatherData(
     humidity: current.variables(1)!.value(), // relative_humidity_2m
     windSpeed: current.variables(2)!.value(), // wind_speed_10m
     rainProbability: current.variables(3)!.value(), // precipitation
+    uvIndex: daily.variables(0)!.value(), // uv_index_max
     hourly: hourlyTimes.map((time, i) => ({
       time,
       temperature: hourly.variables(0)!.valuesArray()![i], // temperature_2m
@@ -150,45 +128,31 @@ export async function getWeatherData(
   };
 }
 
-export async function getWeather(
-  lat: number,
-  lon: number,
-): Promise<WeatherData> {
-  const cacheKey = buildCacheKey(lat, lon);
-
-  // check cache first - check if there is any duplicate key
-  const cached = await db
-    .select()
-    .from(weatherCache)
-    .where(eq(weatherCache.id, cacheKey))
-    .limit(1);
-
-  if (cached.length > 0) {
-    const entry = cached[0];
-    // if its not expired, then return the cached data
-    if (new Date() < new Date(entry.expiresAt)) {
-      return entry.data as WeatherData;
-    }
-  }
-
-  // if no cached data or cached data is expired, fetch fresh data
-  const data = await getWeatherData(lat, lon);
-  const expiresAt = new Date(Date.now() + CACHE_TTL_MINUTES * 60 * 1000);
-
-  await db
-    .insert(weatherCache)
-    .values({
-      id: cacheKey,
-      lat,
-      lon,
-      data,
-      expiresAt,
-    })
-    // upsert
-    .onConflictDoUpdate({
-      target: weatherCache.id,
-      set: { data, fetchedAt: new Date(), expiresAt },
-    });
-
-  return data;
+async function fetchFromExternalAPI(lat: number, lon: number) {
+  // Using open-meteo https://open-meteo.com/en/docs
+  const OPEN_METEO_URL = "https://api.open-meteo.com/v1/forecast";
+  const params = new URLSearchParams({
+    latitude: lat.toString(),
+    longitude: lon.toString(),
+    // The info from open meteo we need.
+    daily: ["uv_index_max"],
+    current: [
+      "temperature_2m",
+      "relative_humidity_2m",
+      "wind_speed_10m",
+      "precipitation_probability",
+    ].join(","),
+    hourly: [
+      "temperature_2m",
+      "relative_humidity_2m",
+      "wind_speed_10m",
+      "uv_index",
+      "precipitation_probability",
+    ].join(","),
+    forecast_days: "1",
+    timezone: "auto",
+  });
+  const responses = await fetchWeatherApi(OPEN_METEO_URL, params);
+  // don't know why open meteo returns an array of responses. only need the first response
+  return responses[0];
 }
