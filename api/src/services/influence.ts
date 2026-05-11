eb02c27287257f37af0d146b63cc3d0a745e1cf0
Eniola Ajayi <ajayieniola16@gmail.com>
2026-05-11 07:06:11 +0100

    refactor influence services

--- api/src/services/influence.ts
+++ api/src/services/influence.ts
@@ -1,3 +1,4 @@
+import { getWeather, HourlyForecast } from "./weatherForecast";
 export type SunDryConditions = {
   temperature: number;
   humidity: number;
@@ -15,19 +16,46 @@ export type SunDryConditionsSnapshot = {
 
 export async function getCurrentConditions() {}
 
-export async function getHourlyConditions() {}
+export function getHourlyConditions(forecast: HourlyForecast[]) {
+  return forecast.map((h) => {
+    return {
+      time: h.time,
+      temperature: h.temperature,
+      humidity: h.humidity,
+      windSpeed: h.windSpeed,
+      uvIndex: h.uvIndex,
+      rainProbability: h.rainProbability,
+    };
+  });
+}
+
 export async function getSunDryConditionsSnapshot(
   latitude: number,
   longitude: number,
 ): Promise<SunDryConditionsSnapshot> {
+  const forecast = await getWeather(latitude, longitude);
+  // will probably run some checks here
+  if (!forecast) {
+    return {
+      current: {
+        temperature: 0,
+        humidity: 0,
+        windSpeed: 0,
+        uvIndex: 0,
+        rainProbability: 0,
+      },
+      hourly: [],
+    };
+  }
+
   return {
     current: {
-      temperature: 0,
-      humidity: 0,
-      windSpeed: 0,
-      uvIndex: 0,
-      rainProbability: 0,
+      temperature: forecast.temperature,
+      humidity: forecast.humidity,
+      windSpeed: forecast.windSpeed,
+      uvIndex: forecast.uvIndex,
+      rainProbability: forecast.rainProbability,
     },
-    hourly: [],
+    hourly: getHourlyConditions(forecast.hourly),
   };
 }
